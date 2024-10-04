680062fdcab3754142859f1142b7e1ad1b54cb61
Kei-1111 <kei_02_1111@icloud.com>
2024-10-04 13:23:34 +0900

    refactor: detekt warning point

--- app/src/main/java/com/example/withmo/ui/component/AppItem.kt
+++ app/src/main/java/com/example/withmo/ui/component/AppItem.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
