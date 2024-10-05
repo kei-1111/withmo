f2e7a460139d6c51ad1fe0d76390e9caa2c12a0e
Kei-1111 <kei_02_1111@icloud.com>
2024-10-05 12:56:12 +0900

    update: refactored navigation by implementing new method and replacing existing approach

--- app/src/main/java/com/example/withmo/ui/AppContent.kt
+++ app/src/main/java/com/example/withmo/ui/AppContent.kt
@@ -3,15 +3,23 @@ package com.example.withmo.ui
 import android.os.Build
 import android.widget.FrameLayout
 import androidx.annotation.RequiresApi
-import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.withmo.domain.model.AppInfo
+import com.example.withmo.domain.model.Screen
 import com.example.withmo.ui.screens.home.HomeScreen
 import com.example.withmo.ui.screens.setting.SettingsScreen
 import com.unity3d.player.UnityPlayer
@@ -20,13 +28,12 @@ import kotlinx.collections.immutable.ImmutableList
 @RequiresApi(Build.VERSION_CODES.R)
 @Composable
 fun AppContent(
-    uiState: Boolean,
     unityPlayer: UnityPlayer?,
-    navigateToSettingScreen: () -> Unit,
-    navigateToHomeScreen: () -> Unit,
     appList: ImmutableList<AppInfo>,
     modifier: Modifier = Modifier,
 ) {
+    var currentScreen by remember { mutableStateOf<Screen>(Screen.Home) }
+
     Surface(
         modifier = modifier,
     ) {
@@ -37,27 +44,34 @@ fun AppContent(
             )
         }
 
-        AnimatedVisibility(
-            visible = !uiState,
-        ) {
-            HomeScreen(
-                navigateToSettingScreen = navigateToSettingScreen,
-                appList = appList,
-            )
-        }
+        AnimatedContent(
+            targetState = currentScreen,
+            transitionSpec = {
+                when (currentScreen) {
+                    is Screen.Home -> {
+                        fadeIn().togetherWith(slideOutVertically { it })
+                    }
 
-        AnimatedVisibility(
-            visible = uiState,
-            enter = slideInVertically(
-                initialOffsetY = { it },
-            ),
-            exit = slideOutVertically(
-                targetOffsetY = { it },
-            ),
-        ) {
-            SettingsScreen(
-                navigateToHomeScreen = navigateToHomeScreen,
-            )
+                    is Screen.Settings -> {
+                        slideInVertically { it }.togetherWith(fadeOut())
+                    }
+                }
+            },
+        ) { targetState ->
+            when (targetState) {
+                is Screen.Home -> {
+                    HomeScreen(
+                        navigateToSettingScreen = { currentScreen = Screen.Settings },
+                        appList = appList,
+                    )
+                }
+
+                is Screen.Settings -> {
+                    SettingsScreen(
+                        navigateToHomeScreen = { currentScreen = Screen.Home },
+                    )
+                }
+            }
         }
     }
 }
