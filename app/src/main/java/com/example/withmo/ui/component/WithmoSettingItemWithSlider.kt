5a0a82f9b06d70d288fa933517a3a949fd59f660
Kei-1111 <kei_02_1111@icloud.com>
2024-10-09 21:22:21 +0900

    fix: Revert Slider appearance to pre-Material3 1.3.0 design

--- app/src/main/java/com/example/withmo/ui/component/WithmoSettingItemWithSlider.kt
+++ app/src/main/java/com/example/withmo/ui/component/WithmoSettingItemWithSlider.kt
@@ -1,14 +1,28 @@
 package com.example.withmo.ui.component
 
+import androidx.compose.foundation.indication
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSizeIn
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Surface
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import com.example.withmo.ui.theme.UiConfig
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WithmoSettingItemWithSlider(
     title: String,
@@ -18,6 +32,8 @@ fun WithmoSettingItemWithSlider(
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
 ) {
+    val interactionSource = remember { MutableInteractionSource() }
+
     Surface(
         modifier = modifier,
         color = MaterialTheme.colorScheme.surfaceContainer,
@@ -35,6 +51,37 @@ fun WithmoSettingItemWithSlider(
                 onValueChange = onValueChange,
                 enabled = enabled,
                 valueRange = valueRange,
+                interactionSource = interactionSource,
+                modifier = Modifier
+                    .semantics { contentDescription = "Localized Description" }
+                    .requiredSizeIn(
+                        minWidth = UiConfig.SliderThumbSize,
+                        minHeight = UiConfig.SliderTrackHeight
+                    ),
+                thumb = {
+                    val modifier = Modifier
+                        .size(UiConfig.SliderThumbSize)
+                        .shadow(UiConfig.SliderShadowElevation, CircleShape, clip = false)
+                        .indication(
+                            interactionSource = interactionSource,
+                            indication = ripple(
+                                bounded = false,
+                                radius = UiConfig.SliderThumbSize
+                            )
+                        )
+                    SliderDefaults.Thumb(
+                        interactionSource = interactionSource,
+                        modifier = modifier
+                    )
+                },
+                track = {
+                    val modifier = Modifier
+                        .height(UiConfig.SliderTrackHeight)
+                    SliderDefaults.Track(
+                        sliderState = it,
+                        modifier = modifier,
+                    )
+                }
             )
         }
     }
