d7f18f083365c9aff0181b6800f11034cff95629
Kei-1111 <kei_02_1111@icloud.com>
2024-10-16 15:20:05 +0900

    refactor: HomeScreen to separate UI components from event handling logic

--- app/src/main/java/com/example/withmo/ui/screens/home/HomeUiState.kt
+++ app/src/main/java/com/example/withmo/ui/screens/home/HomeUiState.kt
@@ -2,16 +2,17 @@ package com.example.withmo.ui.screens.home
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import com.example.withmo.domain.model.DateTimeInfo
 import com.example.withmo.domain.model.user_settings.UserSettings
 import com.example.withmo.ui.base.UiState
-import java.time.ZonedDateTime
 
 @RequiresApi(Build.VERSION_CODES.O)
 data class HomeUiState(
-    val currentTime: ZonedDateTime = ZonedDateTime.now(),
+    val currentTime: DateTimeInfo = DateTimeInfo(),
     val isFinishSplashScreen: Boolean = false,
     val isShowScaleSlider: Boolean = false,
     val isExpandPopup: Boolean = false,
+    val isBottomSheetOpened: Boolean = false,
     val appSearchQuery: String = "",
     val currentUserSettings: UserSettings = UserSettings(),
 ) : UiState
