16215747b4dbbcdc421b401a2c00eb6ab2fae3ba
Kei-1111 <kei_02_1111@icloud.com>
2024-10-17 23:15:33 +0900

    add: Pager for content navigation and implement page switching functionality

--- app/src/main/java/com/example/withmo/ui/screens/home/HomeScreenContent.kt
+++ app/src/main/java/com/example/withmo/ui/screens/home/HomeScreenContent.kt
@@ -1,6 +1,7 @@
 package com.example.withmo.ui.screens.home
 
 import android.content.Context
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,8 +11,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Man
@@ -22,6 +28,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Popup
 import com.example.withmo.domain.model.AppInfo
@@ -46,6 +53,8 @@ fun HomeScreenContent(
     appList: ImmutableList<AppInfo>,
     modifier: Modifier = Modifier,
 ) {
+    val pagerState = rememberPagerState(pageCount = { UiConfig.PageCount })
+
     Box(
         modifier = modifier,
     ) {
@@ -72,29 +81,39 @@ fun HomeScreenContent(
             }
             Column(
                 modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Bottom,
-                horizontalAlignment = Alignment.End,
             ) {
-                Column(
-                    modifier = Modifier.padding(UiConfig.MediumPadding),
-                    verticalArrangement = Arrangement.spacedBy(UiConfig.LargePadding),
-                ) {
-                    if (uiState.currentUserSettings.sideButtonSettings.isScaleSliderButtonShown) {
-                        WithmoIconButton(
-                            onClick = {
-                                UnitySendMessage("Slidermaneger", "ShowSlider", "")
-                                onEvent(HomeUiEvent.SetShowScaleSlider(true))
-                            },
-                            icon = Icons.Default.Man,
-                        )
-                    }
-                    if (uiState.currentUserSettings.sideButtonSettings.isSortButtonShown) {
-                        WithmoIconButton(
-                            onClick = { onEvent(HomeUiEvent.SetPopupExpanded(!uiState.isExpandPopup)) },
-                            icon = Icons.Default.Tune,
-                        )
+                HorizontalPager(
+                    state = pagerState,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(UiConfig.DefaultWeight),
+                ) { page ->
+                    when (page) {
+                        0 -> {
+                            DisplayModelContent(
+                                isScaleSliderButtonShown =
+                                uiState.currentUserSettings.sideButtonSettings.isScaleSliderButtonShown,
+                                isSortButtonShown =
+                                uiState.currentUserSettings.sideButtonSettings.isSortButtonShown,
+                                showScaleSlider = {
+                                    UnitySendMessage("Slidermaneger", "ShowSlider", "")
+                                    onEvent(HomeUiEvent.SetShowScaleSlider(true))
+                                },
+                                popupExpand = {
+                                    onEvent(HomeUiEvent.SetPopupExpanded(!uiState.isExpandPopup))
+                                },
+                            )
+                        }
+
+                        1 -> {
+                        }
                     }
                 }
+                PageIndicator(
+                    pageCount = pagerState.pageCount,
+                    currentPage = pagerState.currentPage,
+                    modifier = Modifier.fillMaxWidth(),
+                )
                 RowAppList(
                     context = context,
                     appList = appList,
@@ -110,6 +129,69 @@ fun HomeScreenContent(
                     sortType = uiState.currentUserSettings.sortType,
                 )
             }
+        }
+    }
+}
+
+@Composable
+private fun PageIndicator(
+    pageCount: Int,
+    currentPage: Int,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .wrapContentHeight()
+            .padding(bottom = UiConfig.SmallPadding),
+        horizontalArrangement = Arrangement.Center,
+    ) {
+        repeat(pageCount) { iteration ->
+            val color =
+                if (currentPage == iteration) {
+                    MaterialTheme.colorScheme.onSurface
+                } else {
+                    MaterialTheme.colorScheme.onSurface.copy(alpha = UiConfig.DisabledContentAlpha)
+                }
+            Box(
+                modifier = Modifier
+                    .padding(horizontal = UiConfig.MediumPadding)
+                    .clip(CircleShape)
+                    .background(color)
+                    .size(UiConfig.PageIndicatorSize),
+            )
+        }
+    }
+}
+
+@Composable
+private fun DisplayModelContent(
+    isScaleSliderButtonShown: Boolean,
+    isSortButtonShown: Boolean,
+    showScaleSlider: () -> Unit,
+    popupExpand: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(
+                horizontal = UiConfig.MediumPadding,
+                vertical = UiConfig.ExtraSmallPadding,
+            ),
+        verticalArrangement = Arrangement.spacedBy(UiConfig.LargePadding, Alignment.Bottom),
+        horizontalAlignment = Alignment.End,
+    ) {
+        if (isScaleSliderButtonShown) {
+            WithmoIconButton(
+                onClick = showScaleSlider,
+                icon = Icons.Default.Man,
+            )
+        }
+        if (isSortButtonShown) {
+            WithmoIconButton(
+                onClick = popupExpand,
+                icon = Icons.Default.Tune,
+            )
         }
     }
 }
