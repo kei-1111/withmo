f9a88e29ddcdff02c7ac8ca99387db18e20569a4
kei-1111 <kei_02_1111@icloud.com>
2025-04-17 21:16:33 +0900

    feat: Widget編集中は、クリックできないように変更

--- app/src/main/java/io/github/kei_1111/withmo/ui/component/WithmoWidget.kt
+++ app/src/main/java/io/github/kei_1111/withmo/ui/component/WithmoWidget.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
@@ -18,6 +19,7 @@ import androidx.compose.material.icons.rounded.Close
 import androidx.compose.material.icons.rounded.ZoomOutMap
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -26,6 +28,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.Dp
@@ -153,30 +156,38 @@ fun WithmoWidget(
             modifier = Modifier.align(Alignment.Center),
         )
         if (isEditMode) {
-            Icon(
-                imageVector = Icons.Rounded.Close,
-                contentDescription = null,
-                modifier = Modifier
-                    .padding(Paddings.Tiny)
-                    .background(MaterialTheme.colorScheme.surface, CircleShape)
-                    .padding(Paddings.Tiny)
-                    .align(Alignment.TopEnd)
-                    .size(BadgeSizes.Medium)
-                    .clickable { deleteWidget() },
-                tint = MaterialTheme.colorScheme.onSurface,
-            )
-            Icon(
-                imageVector = Icons.Rounded.ZoomOutMap,
-                contentDescription = null,
-                modifier = Modifier
-                    .padding(Paddings.Tiny)
-                    .background(MaterialTheme.colorScheme.surface, CircleShape)
-                    .padding(Paddings.Tiny)
-                    .align(Alignment.BottomEnd)
-                    .size(BadgeSizes.Medium)
-                    .clickable { resizeWidget() },
-                tint = MaterialTheme.colorScheme.onSurface,
-            )
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                shape = MaterialTheme.shapes.medium,
+                color = Color.Transparent,
+            ) {
+                Box {
+                    Icon(
+                        imageVector = Icons.Rounded.Close,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(Paddings.Tiny)
+                            .background(MaterialTheme.colorScheme.surface, CircleShape)
+                            .padding(Paddings.Tiny)
+                            .align(Alignment.TopEnd)
+                            .size(BadgeSizes.Medium)
+                            .clickable { deleteWidget() },
+                        tint = MaterialTheme.colorScheme.onSurface,
+                    )
+                    Icon(
+                        imageVector = Icons.Rounded.ZoomOutMap,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(Paddings.Tiny)
+                            .background(MaterialTheme.colorScheme.surface, CircleShape)
+                            .padding(Paddings.Tiny)
+                            .align(Alignment.BottomEnd)
+                            .size(BadgeSizes.Medium)
+                            .clickable { resizeWidget() },
+                        tint = MaterialTheme.colorScheme.onSurface,
+                    )
+                }
+            }
         }
     }
 }
