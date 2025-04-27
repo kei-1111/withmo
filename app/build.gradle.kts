48bb31b5b80277d3992f8c65b61b03a23cee2cd6
kei-1111 <kei_02_1111@icloud.com>
2025-04-28 01:35:19 +0900

    other: リリース審査のため、バージョンコードの変更

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "io.github.kei_1111.withmo"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
