493f102ee972a437c361bcacd35497651cce27fd
kei-1111 <kei_02_1111@icloud.com>
2025-05-12 01:10:50 +0900

    other: リリースのためにバージョンコードを変更

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "io.github.kei_1111.withmo"
         minSdk = 24
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
