c389560f937b149f4a457c023014b376963ff5cf
kei-1111 <kei_02_1111@icloud.com>
2025-05-31 13:22:22 +0900

    refactor: core.serviceモジュールにカスタムプラグインを適応

--- core/service/build.gradle.kts
+++ core/service/build.gradle.kts
@@ -1,31 +1,13 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.hilt)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.ksp)
+    alias(libs.plugins.withmo.android.library)
+    alias(libs.plugins.withmo.hilt)
 }
 
 android {
     namespace = "io.github.kei_1111.withmo.core.service"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 24
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
-    implementation(libs.androidx.activity.compose)
-    ksp(libs.hilt.android.compiler)
-    implementation(libs.hilt.android)
     implementation(projects.core.common)
     implementation(projects.core.domain)
     implementation(projects.core.model)
