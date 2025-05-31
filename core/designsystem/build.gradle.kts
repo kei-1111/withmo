651326595d299367f82ccd676b1229d0077ab41f
kei-1111 <kei_02_1111@icloud.com>
2025-05-31 13:14:15 +0900

    refactor: core.designsystemモジュールにカスタムプラグインを適応

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -1,39 +1,18 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.compose.compiler)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.withmo.android.library.compose)
 }
 
 android {
     namespace = "io.github.kei_1111.withmo.core.designsystem"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 24
-    }
-
-    buildFeatures {
-        compose = true
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
     implementation(libs.accompanist.drawablepainter)
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.material.icons.extended)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.kotlinx.collections.immutable)
     implementation(projects.core.common)
     implementation(projects.core.model)
