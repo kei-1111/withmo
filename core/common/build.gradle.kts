fdad4371cae138d1c17aa0c4d52ccae3129b746f
kei-1111 <kei_02_1111@icloud.com>
2025-05-31 13:06:09 +0900

    refactor: core.commonモジュールにカスタムプラグインを適応

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -1,34 +1,15 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.hilt)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.ksp)
+    alias(libs.plugins.withmo.android.library)
+    alias(libs.plugins.withmo.hilt)
 }
 
 android {
     namespace = "io.github.kei_1111.withmo.core.common"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 24
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 val unityLibraryLibsDir = project(":unityLibrary").projectDir.resolve("libs")
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    ksp(libs.hilt.android.compiler)
-    implementation(libs.hilt.android)
     implementation(projects.unityLibrary)
     implementation(fileTree(mapOf("dir" to unityLibraryLibsDir, "include" to listOf("*.jar"))))
 }
