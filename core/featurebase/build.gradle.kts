cd1aa286e633aa63461eb6f500ce33b40f2386ee
kei-1111 <kei_02_1111@icloud.com>
2025-05-31 13:18:20 +0900

    refactor: core.featurebaseモジュールにカスタムプラグインを適応

--- core/featurebase/build.gradle.kts
+++ core/featurebase/build.gradle.kts
@@ -1,23 +1,9 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.withmo.android.library)
 }
 
 android {
     namespace = "io.github.kei_1111.withmo.core.featurebase"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 24
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
