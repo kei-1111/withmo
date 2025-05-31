d458fe3d6d3663df3159db19167b80426b70a5d4
kei-1111 <kei_02_1111@icloud.com>
2025-05-31 13:37:52 +0900

    refactor: feature.settingモジュールにカスタムプラグインを適応

--- feature/setting/build.gradle.kts
+++ feature/setting/build.gradle.kts
@@ -1,46 +1,13 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.compose.compiler)
-    alias(libs.plugins.hilt)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.ksp)
+    alias(libs.plugins.withmo.android.feature)
 }
 
 android {
     namespace = "io.github.kei_1111.withmo.feature.setting"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 24
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 
 dependencies {
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.hilt.navigation.compose)
-    implementation(libs.androidx.material.icons.extended)
-    implementation(libs.androidx.material3)
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    ksp(libs.hilt.android.compiler)
-    implementation(libs.hilt.android)
-    implementation(libs.kotlinx.collections.immutable)
-    implementation(projects.core.common)
-    implementation(projects.core.designsystem)
-    implementation(projects.core.domain)
-    implementation(projects.core.featurebase)
-    implementation(projects.core.model)
     implementation(projects.core.util)
 }
