2eff837f29db7acc26c67a883a54ec3171fc4178
kei-1111 <kei_02_1111@icloud.com>
2025-09-14 23:49:39 +0900

    feat: AppIconSettingsで新たなBaseViewModelを使用するように変更

--- feature/setting/src/main/kotlin/io/github/kei_1111/withmo/feature/setting/app_icon/AppIconSettingsViewModel.kt
+++ feature/setting/src/main/kotlin/io/github/kei_1111/withmo/feature/setting/app_icon/AppIconSettingsViewModel.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.github.kei_1111.withmo.core.domain.usecase.GetAppIconSettingsUseCase
 import io.github.kei_1111.withmo.core.domain.usecase.SaveAppIconSettingsUseCase
-import io.github.kei_1111.withmo.core.featurebase.BaseViewModel
+import io.github.kei_1111.withmo.core.featurebase.BaseViewModelV2
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -13,9 +13,10 @@ import javax.inject.Inject
 class AppIconSettingsViewModel @Inject constructor(
     private val getAppIconSettingsUseCase: GetAppIconSettingsUseCase,
     private val saveAppIconSettingsUseCase: SaveAppIconSettingsUseCase,
-) : BaseViewModel<AppIconSettingsState, AppIconSettingsAction, AppIconSettingsEffect>() {
+) : BaseViewModelV2<AppIconSettingsViewModelState, AppIconSettingsState, AppIconSettingsAction, AppIconSettingsEffect>() {
 
-    override fun createInitialState(): AppIconSettingsState = AppIconSettingsState()
+    override fun createInitialViewModelState() = AppIconSettingsViewModelState()
+    override fun createInitialState() = AppIconSettingsState()
 
     init {
         observeAppIconSettings()
@@ -24,7 +25,7 @@ class AppIconSettingsViewModel @Inject constructor(
     private fun observeAppIconSettings() {
         viewModelScope.launch {
             getAppIconSettingsUseCase().collect { appIconSettings ->
-                updateState {
+                updateViewModelState {
                     copy(
                         appIconSettings = appIconSettings,
                         initialAppIconSettings = appIconSettings,
@@ -34,43 +35,34 @@ class AppIconSettingsViewModel @Inject constructor(
         }
     }
 
-    private fun saveAppIconSettings() {
-        updateState { copy(isSaveButtonEnabled = false) }
-        viewModelScope.launch {
-            try {
-                saveAppIconSettingsUseCase(state.value.appIconSettings)
-                sendEffect(AppIconSettingsEffect.ShowToast("保存しました"))
-                sendEffect(AppIconSettingsEffect.NavigateBack)
-            } catch (e: Exception) {
-                Log.e(TAG, "Failed to save app icon settings", e)
-                sendEffect(AppIconSettingsEffect.ShowToast("保存に失敗しました"))
-            }
-        }
-    }
-
     override fun onAction(action: AppIconSettingsAction) {
         when (action) {
             is AppIconSettingsAction.OnAppIconShapeRadioButtonClick -> {
-                updateState {
+                updateViewModelState {
                     val updatedAppIconSettings = appIconSettings.copy(appIconShape = action.appIconShape)
-                    copy(
-                        appIconSettings = updatedAppIconSettings,
-                        isSaveButtonEnabled = updatedAppIconSettings != initialAppIconSettings,
-                    )
+                    copy(appIconSettings = updatedAppIconSettings)
                 }
             }
 
             is AppIconSettingsAction.OnRoundedCornerPercentSliderChange -> {
-                updateState {
+                updateViewModelState {
                     val updatedAppIconSettings = appIconSettings.copy(roundedCornerPercent = action.roundedCornerPercent)
-                    copy(
-                        appIconSettings = updatedAppIconSettings,
-                        isSaveButtonEnabled = updatedAppIconSettings != initialAppIconSettings,
-                    )
+                    copy(appIconSettings = updatedAppIconSettings)
                 }
             }
 
-            is AppIconSettingsAction.OnSaveButtonClick -> saveAppIconSettings()
+            is AppIconSettingsAction.OnSaveButtonClick -> {
+                viewModelScope.launch {
+                    try {
+                        saveAppIconSettingsUseCase(state.value.appIconSettings)
+                        sendEffect(AppIconSettingsEffect.ShowToast("保存しました"))
+                        sendEffect(AppIconSettingsEffect.NavigateBack)
+                    } catch (e: Exception) {
+                        Log.e(TAG, "Failed to save app icon settings", e)
+                        sendEffect(AppIconSettingsEffect.ShowToast("保存に失敗しました"))
+                    }
+                }
+            }
 
             is AppIconSettingsAction.OnBackButtonClick -> sendEffect(AppIconSettingsEffect.NavigateBack)
         }
