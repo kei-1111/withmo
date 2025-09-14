fb122f883f56e7067a943f2c0df77be05e772fef
kei-1111 <kei_02_1111@icloud.com>
2025-09-15 00:08:34 +0900

    feat: ClockSettingsで新たなBaseViewModelを使用するように変更

--- feature/setting/src/main/kotlin/io/github/kei_1111/withmo/feature/setting/clock/ClockSettingsState.kt
+++ feature/setting/src/main/kotlin/io/github/kei_1111/withmo/feature/setting/clock/ClockSettingsState.kt
@@ -5,6 +5,5 @@ import io.github.kei_1111.withmo.core.model.user_settings.ClockSettings
 
 data class ClockSettingsState(
     val clockSettings: ClockSettings = ClockSettings(),
-    val initialClockSettings: ClockSettings = ClockSettings(),
     val isSaveButtonEnabled: Boolean = false,
 ) : State
