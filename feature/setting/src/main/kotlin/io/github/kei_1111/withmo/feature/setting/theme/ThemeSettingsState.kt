a7d8ae01a38be492f97e6b98fbb17c3165587423
kei-1111 <kei_02_1111@icloud.com>
2025-09-17 13:47:32 +0900

    feat: ThemeSettingsで新たなBaseViewModelを使用するように変更

--- feature/setting/src/main/kotlin/io/github/kei_1111/withmo/feature/setting/theme/ThemeSettingsState.kt
+++ feature/setting/src/main/kotlin/io/github/kei_1111/withmo/feature/setting/theme/ThemeSettingsState.kt
@@ -5,6 +5,5 @@ import io.github.kei_1111.withmo.core.model.user_settings.ThemeSettings
 
 data class ThemeSettingsState(
     val themeSettings: ThemeSettings = ThemeSettings(),
-    val initialThemeSettings: ThemeSettings = ThemeSettings(),
     val isSaveButtonEnabled: Boolean = false,
 ) : State
---
+++ feature/setting/src/main/kotlin/io/github/kei_1111/withmo/feature/setting/theme/ThemeSettingsViewModelState.kt
@@ -0,0 +1,14 @@
+package io.github.kei_1111.withmo.feature.setting.theme
+
+import io.github.kei_1111.withmo.core.featurebase.ViewModelState
+import io.github.kei_1111.withmo.core.model.user_settings.ThemeSettings
+
+data class ThemeSettingsViewModelState(
+    val themeSettings: ThemeSettings = ThemeSettings(),
+    val initialThemeSettings: ThemeSettings = ThemeSettings(),
+) : ViewModelState<ThemeSettingsState> {
+    override fun toState() = ThemeSettingsState(
+        themeSettings = themeSettings,
+        isSaveButtonEnabled = themeSettings != initialThemeSettings,
+    )
+}
