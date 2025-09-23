b849aec18eeb1c68da17f7db588794e8a00403b1
kei-1111 <kei_02_1111@icloud.com>
2025-09-23 18:52:54 +0900

    refactor: 簡潔に書くように変更

--- app/src/main/kotlin/io/github/kei_1111/withmo/navigation/WithmoNavHost.kt
+++ app/src/main/kotlin/io/github/kei_1111/withmo/navigation/WithmoNavHost.kt
@@ -152,38 +152,16 @@ fun WithmoNavHost(
     }
 }
 
-fun NavBackStackEntry.checkScreen(screen: Screen): Boolean {
-    return destination.route == screen::class.qualifiedName
-}
+fun NavBackStackEntry.checkScreen(screen: Screen): Boolean = destination.route == screen::class.qualifiedName
 
-fun AnimatedContentTransitionScope<NavBackStackEntry>.slideInHorizontallyFrom(from: Screen): EnterTransition {
-    return if (initialState.checkScreen(from)) {
-        slideInHorizontally { it }
-    } else {
-        EnterTransition.None
-    }
-}
+fun AnimatedContentTransitionScope<NavBackStackEntry>.slideInHorizontallyFrom(from: Screen): EnterTransition =
+    if (initialState.checkScreen(from)) slideInHorizontally { it } else EnterTransition.None
 
-fun AnimatedContentTransitionScope<NavBackStackEntry>.slideOutHorizontallyTo(to: Screen): ExitTransition {
-    return if (targetState.checkScreen(to)) {
-        slideOutHorizontally { it }
-    } else {
-        ExitTransition.None
-    }
-}
+fun AnimatedContentTransitionScope<NavBackStackEntry>.slideOutHorizontallyTo(to: Screen): ExitTransition =
+    if (targetState.checkScreen(to)) slideOutHorizontally { it } else ExitTransition.None
 
-fun AnimatedContentTransitionScope<NavBackStackEntry>.slideInVerticallyFrom(from: Screen): EnterTransition {
-    return if (initialState.checkScreen(from)) {
-        slideInVertically { it }
-    } else {
-        EnterTransition.None
-    }
-}
+fun AnimatedContentTransitionScope<NavBackStackEntry>.slideInVerticallyFrom(from: Screen): EnterTransition =
+    if (initialState.checkScreen(from)) slideInVertically { it } else EnterTransition.None
 
-fun AnimatedContentTransitionScope<NavBackStackEntry>.slideOutVerticallyTo(to: Screen): ExitTransition {
-    return if (targetState.checkScreen(to)) {
-        slideOutVertically { it }
-    } else {
-        ExitTransition.None
-    }
-}
+fun AnimatedContentTransitionScope<NavBackStackEntry>.slideOutVerticallyTo(to: Screen): ExitTransition =
+    if (targetState.checkScreen(to)) slideOutVertically { it } else ExitTransition.None
