d5bf7651ce4ad077df6ef0998d6526ee1bdd982e
kei-1111 <kei_02_1111@icloud.com>
2025-09-23 18:04:46 +0900

    refactor: 拡張関数dashedBorderを別ファイルで定義するように変更

--- feature/onboarding/src/main/kotlin/io/github/kei_1111/withmo/feature/onboarding/select_display_model/component/SelectDisplayModelScreenContent.kt
+++ feature/onboarding/src/main/kotlin/io/github/kei_1111/withmo/feature/onboarding/select_display_model/component/SelectDisplayModelScreenContent.kt
@@ -21,20 +21,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawWithContent
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.PathEffect
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.graphics.drawOutline
-import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import io.github.kei_1111.withmo.core.designsystem.component.BodyMediumText
 import io.github.kei_1111.withmo.core.designsystem.component.LabelMediumText
+import io.github.kei_1111.withmo.core.designsystem.component.modifier.dashedBorder
 import io.github.kei_1111.withmo.core.designsystem.component.theme.WithmoTheme
 import io.github.kei_1111.withmo.core.designsystem.component.theme.dimensions.BadgeSizes
 import io.github.kei_1111.withmo.core.designsystem.component.theme.dimensions.Paddings
@@ -151,33 +143,6 @@ private fun SelectDisplayModelArea(
             }
         }
     }
-}
-
-private fun Modifier.dashedBorder(
-    color: Color,
-    shape: Shape,
-    strokeWidth: Dp = 2.dp,
-    dashLength: Dp = 4.dp,
-    gapLength: Dp = 8.dp,
-    cap: StrokeCap = StrokeCap.Round,
-): Modifier = this.drawWithContent {
-    val outline = shape.createOutline(size, layoutDirection, density = this)
-
-    val dashedStroke = Stroke(
-        cap = cap,
-        width = strokeWidth.toPx(),
-        pathEffect = PathEffect.dashPathEffect(
-            intervals = floatArrayOf(dashLength.toPx(), gapLength.toPx()),
-        ),
-    )
-
-    drawContent()
-
-    drawOutline(
-        outline = outline,
-        style = dashedStroke,
-        brush = SolidColor(color),
-    )
 }
 
 @RequiresApi(Build.VERSION_CODES.R)
