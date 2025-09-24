f3e6d201d68b390e719a62f83c161c5fb0558d9a
kei-1111 <kei_02_1111@icloud.com>
2025-09-24 23:25:01 +0900

    refactor: NotificationSettingsが取りうる状態に応じてStateを分けるように変更

--- feature/setting/src/main/kotlin/io/github/kei_1111/withmo/feature/setting/notification/NotificationSettingsState.kt
+++ feature/setting/src/main/kotlin/io/github/kei_1111/withmo/feature/setting/notification/NotificationSettingsState.kt
@@ -3,7 +3,15 @@ package io.github.kei_1111.withmo.feature.setting.notification
 import io.github.kei_1111.withmo.core.featurebase.stateful.State
 import io.github.kei_1111.withmo.core.model.user_settings.NotificationSettings
 
-data class NotificationSettingsState(
-    val notificationSettings: NotificationSettings = NotificationSettings(),
-    val isSaveButtonEnabled: Boolean = false,
-) : State
+sealed interface NotificationSettingsState : State {
+    data object Idle : NotificationSettingsState
+
+    data object Loading : NotificationSettingsState
+
+    data class Stable(
+        val notificationSettings: NotificationSettings = NotificationSettings(),
+        val isSaveButtonEnabled: Boolean = false,
+    ) : NotificationSettingsState
+
+    data class Error(val error: Throwable) : NotificationSettingsState
+}
--- feature/setting/src/main/kotlin/io/github/kei_1111/withmo/feature/setting/notification/NotificationSettingsViewModel.kt
+++ feature/setting/src/main/kotlin/io/github/kei_1111/withmo/feature/setting/notification/NotificationSettingsViewModel.kt
@@ -18,17 +18,17 @@ class NotificationSettingsViewModel @Inject constructor(
 ) : StatefulBaseViewModel<NotificationSettingsViewModelState, NotificationSettingsState, NotificationSettingsAction, NotificationSettingsEffect>() {
 
     override fun createInitialViewModelState() = NotificationSettingsViewModelState()
-    override fun createInitialState() = NotificationSettingsState()
+    override fun createInitialState() = NotificationSettingsState.Idle
+
+    private val notificationSettingsDataStream = getNotificationSettingsUseCase()
 
     init {
-        observeNotificationSettings()
-    }
-
-    private fun observeNotificationSettings() {
         viewModelScope.launch {
-            getNotificationSettingsUseCase().collect { notificationSettings ->
+            updateViewModelState { copy(statusType = NotificationSettingsViewModelState.StatusType.LOADING) }
+            notificationSettingsDataStream.collect { notificationSettings ->
                 updateViewModelState {
                     copy(
+                        statusType = NotificationSettingsViewModelState.StatusType.STABLE,
                         notificationSettings = notificationSettings,
                         initialNotificationSettings = notificationSettings,
                     )
@@ -58,7 +58,7 @@ class NotificationSettingsViewModel @Inject constructor(
             is NotificationSettingsAction.OnSaveButtonClick -> {
                 viewModelScope.launch {
                     try {
-                        saveNotificationSettingsUseCase(state.value.notificationSettings)
+                        saveNotificationSettingsUseCase(_viewModelState.value.notificationSettings)
                         sendEffect(NotificationSettingsEffect.ShowToast("保存しました"))
                         sendEffect(NotificationSettingsEffect.NavigateBack)
                     } catch (e: Exception) {
