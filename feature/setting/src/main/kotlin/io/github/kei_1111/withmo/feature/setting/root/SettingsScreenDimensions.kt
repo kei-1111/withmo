c63b692522c4f67eeafe670fd0806707e094b27f
kei-1111 <kei_02_1111@icloud.com>
2025-09-30 15:12:23 +0900

    refactor: 他のDimensionsを削除

--- feature/setting/src/main/kotlin/io/github/kei_1111/withmo/feature/setting/root/SettingsScreenDimensions.kt
+++
@@ -1,3 +0,0 @@
-package io.github.kei_1111.withmo.feature.setting.root
-
-internal data object SettingsScreenDimensions
--- feature/setting/src/main/kotlin/io/github/kei_1111/withmo/feature/setting/theme/ThemeSettingsScreenDimensions.kt
+++
@@ -1,3 +0,0 @@
-package io.github.kei_1111.withmo.feature.setting.theme
-
-internal data object ThemeSettingsScreenDimensions
